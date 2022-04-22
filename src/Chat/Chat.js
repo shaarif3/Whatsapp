9879ed86a25b3ef90d670f52153b46c8ae3417ee
Shaarif <shaarifshahid@gmail.com>
2022-04-22 15:35:26 +0500

    UI Completed

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -4,11 +4,18 @@ import { Avatar, IconButton } from '@material-ui/core';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import AttachFileOutlinedIcon from '@mui/icons-material/AttachFileOutlined';
+import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined';
+import MicOutlinedIcon from '@mui/icons-material/MicOutlined';
 const Chat = () => {
   const [seed, setSeed] = useState('');
+  const [input, setInput] = useState('');
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log('you typed', input);
+  };
   return (
     <div className='chat'>
       <div className='chat_header'>
@@ -29,8 +36,28 @@ const Chat = () => {
           </IconButton>
         </div>
       </div>
-      <div className='chat_body'></div>
-      <div className='chat_footer'></div>
+      <div className='chat_body'>
+        <p className={`chat_message ${true && 'chat_reciever'}`}>
+          <span className='chat_name'>Shaarif Shahid</span>
+          Hi Guys
+          <span className='chat_timestamp'>2:45pm</span>
+        </p>
+      </div>
+      <div className='chat_footer'>
+        <EmojiEmotionsOutlinedIcon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type='text'
+            placeholder='Type a message...'
+          />
+          <button type='submit' onClick={sendMessage}>
+            Send a message
+          </button>
+        </form>
+        <MicOutlinedIcon />
+      </div>
     </div>
   );
 };
